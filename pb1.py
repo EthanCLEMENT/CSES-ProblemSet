ace5b2a095ebc4707bd19f54ce164aa077706eca
Ethan Clement <80329688+EthanCLEMENT@users.noreply.github.com>
2022-08-20 13:25:35 +0200

    Add files via upload

---
+++ pb1.py
@@ -0,0 +1,11 @@
+def solution():
+    n = int(input())
+    print(n)
+    while n!=1:
+        if n%2==0:
+            n/=2
+        else:
+            n = 3*n+1
+        print(int(n))
+
+solution()
