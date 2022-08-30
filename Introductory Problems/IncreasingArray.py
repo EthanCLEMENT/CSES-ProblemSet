6bca1eb36059c22c5b9ece810fe583676b7d1e69
Ethan Clement <80329688+EthanCLEMENT@users.noreply.github.com>
2022-08-30 11:44:30 +0200

    Update IncreasingArray.py

--- Introductory Problems/IncreasingArray.py
+++ Introductory Problems/IncreasingArray.py
@@ -1,1 +1,10 @@
-
+def solution():
+   n=int(input())
+   ans=0
+   arr=[int(i) for i in input().split()]
+   for i in range(1,n):
+       if arr[i]<arr[i-1]:
+           ans+=(arr[i-1]-arr[i])
+           arr[i]=arr[i-1]
+   return ans
+print(solution())
